743fc83b60f4b47435b459489fd9793a9da3aa72
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-20 09:32:59 +0600

    use middle ware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,29 @@
-import express, { type Request, type Response } from "express";
-const app = express();
-const port = 3000;
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.status(200).json({
+    message: "Root Route Response",
+    Author: "EccTechBD",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  res.status(200).json({
+    message: "User Create Success",
+    body: { name, email },
+  });
 });
 
 app.listen(port, () => {

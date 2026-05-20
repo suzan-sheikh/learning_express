9205b95e4800004c2b428cb9fd4e332f3fbf3405
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-20 10:19:48 +0600

    data create succes

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,41 @@ import express, {
   type Request,
   type Response,
 } from "express";
-
+import { Pool } from "pg";
 const app: Application = express();
 const port = 5000;
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_LnXy5hWQj6UH@ep-winter-mountain-aok27e9d-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            age INT,
+
+            created_at TIMESTAMP DEFAULT NOW(),
+            update_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -19,10 +47,15 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.post("/", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
+  const { name, email, password, age } = req.body;
+
+  const result = await pool.query(`
+    INSERT INTO users(name, email, password, age) VALUES($1, $2, $3, $4) RETURNING *
+    `, [name, email, password, age]);
+
   res.status(200).json({
     message: "User Create Success",
-    body: { name, email },
+    body: { name, email, password, age },
   });
 });
 
